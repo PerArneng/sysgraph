8774d64b2e9a482acc4918cc7d369b72973bc579
Per Arneng <per.arneng@scalebit.com>
2017-10-30 03:55:23 +0100

    prepared parsing

--- src/io/component_diagram_xml_rs_parser.rs
+++ src/io/component_diagram_xml_rs_parser.rs
@@ -6,6 +6,7 @@ use xml::reader::{EventReader, XmlEvent};
 use model::{ComponentDiagram};
 use io::component_diagram_parser::{ComponentDiagramParser};
 
+
 pub struct ComponentDiagramXmlRsParser {
 
 }
@@ -14,6 +15,13 @@ impl ComponentDiagramXmlRsParser {
     pub fn new() -> ComponentDiagramXmlRsParser {
         return ComponentDiagramXmlRsParser {};
     }
+/*
+    fn parse_component_diagram<I>(&self, reader: I) -> Result<ComponentDiagram, String>
+        where I: Iterator<Item=Result<XmlEvent>>  {
+
+        //reader.next();
+
+    }*/
 }
 
 impl ComponentDiagramParser for ComponentDiagramXmlRsParser {
@@ -21,12 +29,14 @@ impl ComponentDiagramParser for ComponentDiagramXmlRsParser {
     fn parse_bytes(&self, spec: &[u8]) -> Result<ComponentDiagram, String> {
         let parser = EventReader::new(spec);
 
+        //self.hi(&(parser.into_iter()));
+
         let mut components_name: &str;
 
         for e in parser {
             match e {
-                Ok(XmlEvent::StartElement { name, .. }) => {
-                    println!("{}", name);
+                Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+                    println!("{} {:?}", name, attributes);
                 }
                 Ok(XmlEvent::EndElement { name }) => {
                     println!("{}", name);
