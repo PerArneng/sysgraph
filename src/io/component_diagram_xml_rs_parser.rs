b1c413cc0d733b567fbed04788467f631cbbbaf4
Per Arneng <per.arneng@scalebit.com>
2017-10-31 21:19:33 +0100

    commented out example code

--- src/io/component_diagram_xml_rs_parser.rs
+++ src/io/component_diagram_xml_rs_parser.rs
@@ -15,7 +15,7 @@ impl ComponentDiagramXmlRsParser {
     pub fn new() -> ComponentDiagramXmlRsParser {
         return ComponentDiagramXmlRsParser {};
     }
-    
+
     fn parse_component_diagram(&self, reader: &EventReader<&[u8]> )
         //-> Result<ComponentDiagram, String>
     {
@@ -31,25 +31,25 @@ impl ComponentDiagramParser for ComponentDiagramXmlRsParser {
         let parser = EventReader::new(spec);
 
         self.parse_component_diagram(&parser);
+        /*
+                let mut components_name: &str;
 
-        let mut components_name: &str;
-
-        for e in parser {
-            match e {
-                Ok(XmlEvent::StartElement { name, attributes, .. }) => {
-                    println!("{} {:?}", name, attributes);
+                for e in parser {
+                    match e {
+                        Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+                            println!("{} {:?}", name, attributes);
+                        }
+                        Ok(XmlEvent::EndElement { name }) => {
+                            println!("{}", name);
+                        }
+                        Err(e) => {
+                            println!("Error: {}", e);
+                            break;
+                        }
+                        _ => {}
+                    }
                 }
-                Ok(XmlEvent::EndElement { name }) => {
-                    println!("{}", name);
-                }
-                Err(e) => {
-                    println!("Error: {}", e);
-                    break;
-                }
-                _ => {}
-            }
-        }
-
+        */
         return Ok(ComponentDiagram::new("hello"));
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 #![allow(unused_mut)]
-
+#![allow(unused_imports)]
 extern crate xml;
 
 pub mod output_type;
